62a33385f2140c19f35f002cdf30da94ae3e9fbd
Felipe-Belotto <felipeeduardo817@gmail.com>
2023-10-19 15:06:17 -0300

    adicionando cabecalho

---
+++ ts/src/app/cabecalho/cabecalho.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'cabecalho',
+  templateUrl: './cabecalho.component.html',
+  styleUrls: ['./cabecalho.component.scss']
+})
+export class CabecalhoComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
